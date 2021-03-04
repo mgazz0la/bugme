9de156bf9d7f082609a5222ca628028fdc3264be
Michael Gazzola <michaelagazzola@gmail.com>
2021-03-04 18:20:58 -0500

    Add some stubs for the mmu.

---
+++ src/mmu.cc
@@ -0,0 +1,18 @@
+#include "mmu.hh"
+#include "cpu.hh"
+
+namespace gbc {
+
+Mmu::Mmu(Cpu& cpu) : cpu_(cpu) {
+  memory_ = std::vector<std::uint8_t>(0x10000);
+}
+
+std::uint8_t Mmu::read(std::uint16_t addr) const {
+  return memory_.at(addr);
+}
+
+void Mmu::write(std::uint16_t addr, std::uint8_t byte) {
+  memory_.at(addr) = byte;
+}
+
+} // gbc
---
+++ src/mmu.hh
@@ -0,0 +1,24 @@
+#ifndef GBC_MMU_H
+#define GBC_MMU_H
+
+#include <cstdint>
+#include <vector>
+
+namespace gbc {
+
+class Cpu;
+
+class Mmu {
+public:
+  Mmu(Cpu& cpu);
+
+  uint8_t read(std::uint16_t addr) const;
+  void write(std::uint16_t addr, std::uint8_t byte);
+private:
+  Cpu& cpu_;
+  std::vector<std::uint8_t> memory_;
+};
+
+} // gbc
+
+#endif
